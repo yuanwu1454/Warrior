f9e64a31fdfa7202e74ef352eb21f677ada98c4c
martin.yuan <1454290602@qq.com>
2026-06-20 20:06:23 +0800

    BindingInput

--- Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -2,12 +2,16 @@
 
 
 #include "Characters/WarriorHeroCharacter.h"
-
+#include "Components/Input/WarriorInputComponent.h"
 #include "WarriorDebugHelper.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "WarriorGameplayTags.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+
 
 
 AWarriorHeroCharacter::AWarriorHeroCharacter()
@@ -38,4 +42,69 @@ void AWarriorHeroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	Debug::Print(TEXT("Working"));
+}
+
+void AWarriorHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	checkf(InputConfigDataAsset, TEXT("Forgot to assign a valid data asset as input config"));
+
+	ULocalPlayer* LocalPlayer = GetController<APlayerController>()->GetLocalPlayer();
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+	check(Subsystem);
+	Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext,0);
+	UWarriorInputComponent* WarriorInputComponent =  CastChecked<UWarriorInputComponent>(PlayerInputComponent);
+	WarriorInputComponent->BindNativeInputAction(InputConfigDataAsset, WarriorGameplayTags::InputTag_Move, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+	WarriorInputComponent->BindNativeInputAction(InputConfigDataAsset, WarriorGameplayTags::InputTag_Look, ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
+}
+
+void AWarriorHeroCharacter::Input_Move(const FInputActionValue& InputActionValue)
+{
+	// 1. 从输入动作中读取二维向量：X=左右(A/D)，Y=前后(W/S)，范围[-1,1]
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+	// 2. 获取控制器控制旋转，只保留Yaw（水平左右转向），Pitch/Roll置0，只算水平平面方向
+	// Controller->GetControlRotation() = 相机朝向
+	const FRotator MovementRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+
+	// ========== 前后移动（W/S 对应 Y 轴） ==========
+	if (MovementVector.Y != 0.f)
+	{
+		// 将世界前向向量，按相机水平旋转偏移，得到角色自身前向（面朝相机前方走）
+		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+		// 添加移动输入，Y正数向前、负数向后
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+	}
+
+	// ========== 左右侧移（A/D 对应 X 轴） ==========
+	if (MovementVector.X != 0.f)
+	{
+		// 将世界右向向量，按相机水平旋转偏移，得到角色自身右向（相机视角左右横移）
+		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
+		// 添加移动输入，X正数向右、负数向左		
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+
+}
+
+
+
+void AWarriorHeroCharacter::Input_Look(const FInputActionValue& InputActionValue)
+{
+	// 从增强输入中提取二维视角向量
+	// X：鼠标左右移动 / 右摇杆左右（控制水平转向 Yaw）
+	// Y：鼠标上下移动 / 右摇杆上下（控制俯仰抬头 Pitch）
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+
+	// 水平左右转头（Yaw）
+	if (LookAxisVector.X != 0.f)
+	{
+		// 给控制器叠加水平旋转输入，正数右转、负数左转
+		AddControllerYawInput(LookAxisVector.X);	
+	}
+
+	// 上下抬头低头（Pitch）
+	if (LookAxisVector.Y != 0.f)
+	{
+		// 给控制器叠加俯仰旋转输入，正数抬头、负数低头
+		AddControllerPitchInput(LookAxisVector.Y);	
+	}
 }
--- Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -8,6 +8,10 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+class UDataAsset_InputConfig;
+
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -19,16 +23,26 @@ public:
 	AWarriorHeroCharacter();
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 private:
 #pragma region Components
-	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,meta=(AllowPrivateAccess="true"))
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Camera",meta=(AllowPrivateAccess="true"))
 	USpringArmComponent* CameraBoom;
 
 // 	C++ 中变量 / 函数标记为 private / protected 时，默认蓝图无法读写、调用；
 // 加上 AllowPrivateAccess="true" 后：
 // C++ 层面依然是私有，C++ 代码外部不能直接访问；
 // 蓝图编辑器可以正常读取、修改、调用 这个私有成员。
-	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,meta=(AllowPrivateAccess="true"))
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Camera",meta=(AllowPrivateAccess="true"))
 	UCameraComponent* FollowCamera;
-	#pragma endregion 
+#pragma endregion
+
+
+#pragma region Input
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="CharacterData",meta=(AllowPrivateAccess="true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+	void Input_Move(const FInputActionValue& InputActionValue);	
+	void Input_Look(const FInputActionValue& InputActionValue);	
+#pragma endregion 
+
 };
