d9faaa93978c2c8b86276a3c145ca037be71ab50
martin.yuan <1454290602@qq.com>
2026-06-21 18:19:43 +0800

    Hero anim Instance

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -3,3 +3,36 @@
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
 
+#include "Characters/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	if (bHasAcceleration)
+	{
+		bShouldEnterRelaxState = false;
+		IdleElapsedTime = 0.f;
+	}else
+	{
+		if (bShouldEnterRelaxState == false)
+		{
+			IdleElapsedTime += DeltaSeconds;
+			if (IdleElapsedTime > EnterRelaxStateThreshold)
+			{
+				bShouldEnterRelaxState = true;
+				IdleElapsedTime = 0.f;
+			}
+		}
+
+		
+	}
+}
--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
-
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -13,5 +13,19 @@ UCLASS()
 class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category= "AnimData|Reference")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category= "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	float IdleElapsedTime=0.f;
 };
