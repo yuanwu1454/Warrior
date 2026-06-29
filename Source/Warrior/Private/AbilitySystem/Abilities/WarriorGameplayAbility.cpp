e5f488be5d4ce1c99bef21e1b243432a3b323ee9
yuanwu1454 <yuanwu1454@users.noreply.github.com>
2026-06-29 16:06:48 +0800

    Set Up Weapon Class

--- Source/Warrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
@@ -3,10 +3,19 @@
 
 #include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
 
+#include "AbilitySystemComponent.h"
+
 void UWarriorGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilitySpec& Spec)
+                                            const FGameplayAbilitySpec& Spec)
 {
 	Super::OnGiveAbility(ActorInfo, Spec);
+	if(AbilityActivationPoilcy == EWarriorAbilityActivationPoilcy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
 }
 
 void UWarriorGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
@@ -14,4 +23,11 @@ void UWarriorGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	if(AbilityActivationPoilcy == EWarriorAbilityActivationPoilcy::OnGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
 }
---
+++ Source/Warrior/Private/Items/Weapons/WarriorWeaponBase.cpp
@@ -0,0 +1,22 @@
+// Martin Yuan All Rights Reserved.
+
+
+#include "Items/Weapons/WarriorWeaponBase.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AWarriorWeaponBase::AWarriorWeaponBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	SetRootComponent(WeaponMesh);
+
+	WeaponCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponCollisionBox"));
+	WeaponCollisionBox->SetupAttachment(GetRootComponent());
+	WeaponCollisionBox->SetBoxExtent(FVector(20.f));
+	WeaponCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
