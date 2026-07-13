033120732e8712da87c3d2cc5f6794e946c14cb1
yuanwu1454 <yuanwu1454@users.noreply.github.com>
2026-07-13 10:26:56 +0800

    Master Anim Layer

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
@@ -3,3 +3,9 @@
 
 #include "AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h"
 
+#include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+
+UWarriorHeroAnimInstance* UWarriorHeroLinkedAnimLayer::GetHeroAnimInstance() const
+{
+	return Cast<UWarriorHeroAnimInstance>(GetOwningComponent()->GetAnimInstance());
+}
--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/WarriorBaseAnimInstance.h"
 #include "WarriorHeroLinkedAnimLayer.generated.h"
 
+class UWarriorHeroAnimInstance;
 /**
  * 
  */
@@ -13,5 +14,7 @@ UCLASS()
 class WARRIOR_API UWarriorHeroLinkedAnimLayer : public UWarriorBaseAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+	UFUNCTION(BlueprintPure, meta=(BlueprintThreadSafe))
+	UWarriorHeroAnimInstance* GetHeroAnimInstance() const;
 };
