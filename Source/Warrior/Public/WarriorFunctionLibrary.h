45eff1319ad03a346679b84f0dc52a32bd448c56
martin.yuan <1454290602@qq.com>
2026-07-19 17:50:07 +0800

    Warrior Function Library

---
+++ Source/Warrior/Public/WarriorFunctionLibrary.h
@@ -0,0 +1,39 @@
+﻿// Martin Yuan All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WarriorFunctionLibrary.generated.h"
+
+UENUM()
+enum class EWarriorConfirmType :uint8
+{
+	Yes,
+	No
+};
+class UWarriorAbilitySystemComponent;
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	static UWarriorAbilitySystemComponent* NativeGetWarriorASCFromActor(AActor* InActor);
+
+	UFUNCTION(BlueprintCallable, Category="Warrior|FunctoinLibrary")
+	static void AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd);
+
+	UFUNCTION(BlueprintCallable, Category="Warrior|FunctoinLibrary")
+	static void RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove);
+
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck);
+
+	UFUNCTION(BlueprintCallable, Category="Warrior|FunctoinLibrary", meta=(DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
+	static void BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EWarriorConfirmType& OutConfirmType);
+	
+};
+
